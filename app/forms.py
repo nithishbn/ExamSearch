29ad2723b5a7316bdd53834af7e1e35413d09884
Nithish Narasimman <nithishthegreat7@gmail.com>
2019-05-23 14:12:06 -0700

    more website styling

--- app/forms.py
+++ app/forms.py
@@ -2,7 +2,7 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 
-
 class SearchForm(FlaskForm):
-    search = StringField('What are you looking for?', validators=[DataRequired()])
+
+    search = StringField(label=" ",validators=[DataRequired()])
     submit = SubmitField('Search')
