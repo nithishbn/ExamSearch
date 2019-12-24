82c99610e4b3c541d1f6f57aad4bf5c2f724e109
Nithish Narasimman <nithishthegreat7@gmail.com>
2019-12-24 00:28:08 -0800

    redo

--- app/config.py
+++
@@ -1,6 +0,0 @@
-import os
-
-class Config(object):
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'secretkey'
-    QUESTIONS_FOLDER = os.path.abspath("static")
-    # SERVER_NAME = 'nithishnarasimman.com'
